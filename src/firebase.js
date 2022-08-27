6e105eeaae6a3b6cf2ddd7de685ddfa294df7ccd
Nasrat Nejat <85827553+nasratnejat@users.noreply.github.com>
2022-08-27 09:52:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import 'firebase/compat/analytics'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBeV7jOX4Dk6GeLRqqrNKh4dDGYDrC5eLY',
-	authDomain: 'twitter-clone-8a807.firebaseapp.com',
-	projectId: 'twitter-clone-8a807',
-	storageBucket: 'twitter-clone-8a807.appspot.com',
-	messagingSenderId: '435457560326',
-	appId: '1:435457560326:web:55e01ce9be24360882d887',
-	measurementId: 'G-34M3MCE6GJ',
+
 }
 
 const app = firebase.initializeApp(firebaseConfig)
